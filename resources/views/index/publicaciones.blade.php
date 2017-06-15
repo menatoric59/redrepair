1bddc02abfe21e20408542d9145be1f29ebc6b5b
vagrant <vagrant@homestead>
2017-06-15 16:55:22 +0000

    Script de google Analytics

--- resources/views/index/publicaciones.blade.php
+++ resources/views/index/publicaciones.blade.php
@@ -11,7 +11,7 @@
         <div class="col-md-4 text-center">
             <h1 class="thin">Libros</h1>
             <br>
-            <a href="http://docs.wixstatic.com/ugd/b39ed3_a5b59285c1e54aea8974b139f97bf209.pdf" target="_blank">
+            <a href="http://docs.wixstatic.com/ugd/b39ed3_a5b59285c1e54aea8974b139f97bf209.pdf" target="_blank" class="img-responsive">
                 {!! Html::image('assets/biblioteca/publicaciones/2.jpg' . '?' . date('Y-m-d h:m') ,'Último cuaderno',['class'=>'center-block','height'=>'400px']) !!}
             </a>
             <br>
@@ -22,7 +22,7 @@
         <div class="col-md-4 text-center">
             <h1 class="thin">Cuadernos</h1>
             <br>
-            <a href="http://docs.wixstatic.com/ugd/b39ed3_fb069af6bdd247abb167af93dbf72e9a.pdf" target="_blank">
+            <a href="http://docs.wixstatic.com/ugd/b39ed3_fb069af6bdd247abb167af93dbf72e9a.pdf" target="_blank" class="img-responsive">
                 {!! Html::image('assets/biblioteca/publicaciones/3.jpg' . '?' . date('Y-m-d h:m') ,'Último cuaderno',['class'=>'center-block','height'=>'400px']) !!}    
             </a>
             
@@ -34,7 +34,7 @@
         <div class="col-md-4 text-center">
             <h1 class="thin">Revista</h1>
             <br>
-            <a href="{{ route('revista') }}">
+            <a href="{{ route('revista') }}" class="img-responsive">
                 {!! Html::image('assets/biblioteca/publicaciones/4.jpg' . '?' . date('Y-m-d h:m') ,'Último cuaderno',['class'=>'center-block','height'=>'400px']) !!}
             </a>
             <br>
