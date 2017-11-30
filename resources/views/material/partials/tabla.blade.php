da7e6962b9c4e161c7e4e925c8c4b313f9e58a97
vagrant <vagrant@homestead>
2017-11-30 19:02:28 +0000

    Corrigiendo la variable reunion en la vista de Material

--- resources/views/material/partials/tabla.blade.php
+++ resources/views/material/partials/tabla.blade.php
@@ -25,7 +25,7 @@
 
             @if( auth()->check() )
                 <td>
-                    <a href="{{ route('reuniones.editar',$reunion->id) }}" class="btn btn-success btn-sm">
+                    <a href="{{ route('reuniones.editar',1) }}" class="btn btn-success btn-sm">
                         <i class="fa fa-pencil"></i>
 
                     </a>
