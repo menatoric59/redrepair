ede396611d659641d3e6f65adf2d1ef076d6a6e5
vagrant <vagrant@homestead>
2017-12-01 18:53:25 +0000

    Subiendo dos talleres de Formacion

--- resources/views/formacion/seminarios.blade.php
+++ resources/views/formacion/seminarios.blade.php
@@ -6,62 +6,72 @@
         <h1 class="text-center thin">Formación Red Mexciteg</h1>
         <br>
 
-        <p>
+        <p class="text-justify">
             Para revertir las relaciones de poder de género, resulta imprescindible la deconstrucción de valores y normas sexistas y androcéntricas; resignificarlas desde una perspectiva más humanista y equitativa. De modo particular, para las mujeres científicas el trabajo es un proyecto de vida que, incluso, puede entrar en conflicto con otros proyectos que históricamente se les han asignado en el ámbito doméstico y familiar. Esto implica entender la Ciencia y la Tecnología como procesos que discurren en la “sociedad del conocimiento”, así que es vital neutralizar las ideas y prácticas en cuanto al sexismo en la educación en un marco que contemple la formación de competencias para el desempeño científico de alto nivel e incluirlo en la formación general y continua de quienes se orientan en el camino científico, con la consiguiente implicación que ello traería para el desarrollo social.
         </p>
 
-       <p>
+       <p class="text-justify">
            Las mujeres se han incorporado de forma masiva al espacio público desde hace tres décadas aproximadamente, sin embargo la segregación vertical y horizontal caracteriza esta presencia así como la falta de visibilidad de las mujeres en la historia del saber, el condicionamiento de estereotipos de género a través de las relaciones entre los/as docentes y estudiantes, la ausencia de la perspectiva de género como eje transversal y el androcentrismo en las ciencias constituyen expresiones del orden de poder de género en la ciencia.
        </p>
-        <p>
+        <p class="text-justify">
             El transcurso natural del tiempo no conlleva a un cambio; el diseño de políticas y legislaciones afirmativas con perspectiva de género constituye un factor necesario, pero no suficiente para deconstruir el orden de poder de género que se produce y se reproduce en lo simbólico, en los sentidos, en las creencias, las tradiciones, las exigencias que constituyen las subjetividades sociales e individuales. Los mecanismos de eficacia del orden de poder patriarcal son hoy más sutiles e invisibles y los hombres y mujeres no siempre son conscientes de su posición en tanto portadores/as y agentes que reproducen dicho poder.
         </p>
 
-        <p>
+        <p class="text-justify">
             Cada vez aflora con más énfasis, la necesidad y pertinencia de una transformación, ya no solo en el plano legal y político, sino en la subjetivación de género, en las nociones de lo que significa para las personas dedicarse a las ciencias y las características de quienes las ejercen. Por ello es imprescindible el desarrollo de estrategias intencionadas al cambio (Blazquez, 2008) asumiendo formas complejas de concebir la formación del ser humano en los contextos educativos. Se deben promover cambios en la subjetividad individual y colectiva que induzcan a una democracia genérica entre mujeres y hombres, en el establecimiento del orden social y en las instituciones que lo posibilitan e impulsan (Lagarde, 2013).
         </p>
         <br><br>
+
         <div class="row">
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_af30241702a84ec2978a3bbf3f605408.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno1.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
+            <div class="col-md-6">
+                <h2 class="text-center">Taller</h2>
+                {!! Html::image('assets/formacion/1.png','Seminario',['class'=>'img-responsive']) !!}
+                <p class="text-justify">
+                    La Red Mexciteg, la Benemérita Universidad Autónoma de Puebla, la Universidad Iberoamericana Puebla y el Consejo Nacional de Ciencia y Tecnología, convocaron a la comunidad académica a participar en el proceso para asistir al Seminario de Formación de Habilidades Profesionales con Perspectiva de Género (4ta edición, antes Taller de Habilidades profesionales para jóvenes científicas) que se realizará del 6 al 9 de septiembre de 2017, Edificio Carolino, BUAP, Puebla.
+                    Este año también realizamos:
+                </p>
+
+                <p class="text-justify">
+                    <a href="http://www.ifuap.buap.mx/eventos/SFHPPG17/" target="_blank">
+                        <i class="fa fa-link fa-fw"></i>
+                        Taller de Habilidades Profesionales para Jóvenes en Ciencia e Ingeniería20-22 de noviembre 2017Universidad de Los Andes, Bogotá, Colombia
+                    </a>
+                </p>
+                <p class="text-justify">
+                    <a href="https://www.youtube.com/watch?v=yuOqS_RpVFQ" target="_blank">
+                        <i class="fa fa-youtube fa-fw"></i>
+                        Taller de Habilidades Profesionales para Jóvenes en Ciencia e Ingeniería
+                    </a>
+                </p>
+
             </div>
-            {{--
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_ffa31b9ac8124019ba11c4eb054c0e28.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno2.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
-            </div>
-            --}}
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_cad40c7f7d514f63813e9defd2d32533.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno3.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
-            </div>
-        </div>
-        <br><br><br><br><br>
-        <div class="row">
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_aa997e8105524f269a7378dfc5cd9690.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno4.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_dfd9f4d1505b4966bad34d1a34fc3c11.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno5.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="http://docs.wixstatic.com/ugd/b39ed3_fb069af6bdd247abb167af93dbf72e9a.pdf" target="_blank">
-                    {!! Html::image('assets/biblioteca/cuadernos/cuaderno6.jpg','',['class'=>'img-responsive'] ) !!}
-                </a>
-            </div>
-        </div>
+            <div class="col-md-6">
+                <h2 class="text-center">Taller</h2>
+                {!! Html::image('assets/formacion/2.png','Seminario',['class'=>'img-responsive']) !!}
+                <p class="text-justify">
+                    En 2016, con el apoyo de CONACyT, BUAP, la Universidad de Guadalajara y la Red Temática Mexicana de Ciencia, Tecnología y Género, realizaremos el III Taller.
+                </p>
+                <p class="text-justify">
+                    Buscando contribuir a una mayor participación de las mujeres en la ciencia, tecnología e innovación, el Grupo de Trabajo Mujeres en la Física de IUPAP, con el apoyo de CONACyT, Mesoamerican Centre for Theoretical Physics, Benemérita Universidad Autónoma de Puebla y otras instituciones, organizó el Taller de habilidades profesionales para jóvenes científicas, orientado a jóvenes latinoamericanas que se encuentran en posgrado o la etapa inicial de su vida científica. En la primera versión se enfocó a disciplinas donde la presencia femenina es muy reducida como son Matemáticas, Ingenierías y Tecnología. Del 30 de julio al 2 de agosto de 2014, las participantes escucharon de investigadoras consolidadas o expertos en el área el cómo desarrollar algunas de sus actividades del quehacer científico, reflexionaron sobre el ambiente de trabajo en el que se desenvuelven, conocieron organismos y programas que favorecen la colaboración en la región y presentaron sus trabajos de investigación. En total asistieron 30 mexicanas, 3 colombianas y una por cada país siguiente: Cuba, Guatemala, Honduras, El Salvador.
+                </p>
+
+                <p class="text-justify">
+                    <a href="http://www.ifuap.buap.mx/eventos/III_THPJC16/" target="_blank">
+                        <i class="fa fa-link fa-fw"></i>
+                        Liga
+                    </a>
+                </p>
+                <p class="text-justify">
+                    <a href="https://www.youtube.com/watch?v=IpZ8Z_arFcA" target="_blank">
+                        <i class="fa fa-youtube fa-fw"></i>
+                        Video
+                    </a>
+                </p>
 
 
+            </div>
+        </div>
     </div>
-
 
 
 @endsection
