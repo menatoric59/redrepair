4aa71f9e7e6f861f70ef10868f543b5e41eef513
vagrant <vagrant@homestead>
2017-12-05 23:17:54 +0000

    Poniendo flores en imagen de Carrusel Nayarit2017

--- resources/views/index/avisos.blade.php
+++ resources/views/index/avisos.blade.php
@@ -30,7 +30,7 @@
             </div>
             <div class="col-md-3 hidden-md-down">
                 <br><br>
-                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FRedMexciteg%2F&tabs=timeline&width=340&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="700" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FRedMexciteg%2F&tabs=timeline&width=340&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
             </div>
         </div>
 
