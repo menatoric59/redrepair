a3ffbfa9d4b219b6d1050f39fb0cd74a88b22d84
vagrant <vagrant@homestead>
2018-02-01 18:50:10 +0000

    Terminando Biblioteca > Catalogo de publicaciones con pink darken-4

---
+++ resources/views/publicaciones/partials/breadcrumbs-biblioteca.blade.php
@@ -0,0 +1,9 @@
+<ol class="breadcrumb pink darken-4 white-text">
+    <li>
+        <a href="{{ route('publicaciones.ultimas') }}" class="white-text">
+            <i class="fa fa-book fa-fw"></i>
+            Biblioteca
+        </a>
+    </li>
+    @yield('links')
+</ol>
--- resources/views/publicaciones/publicaciones.blade.php
+++ resources/views/publicaciones/publicaciones.blade.php
@@ -1,8 +1,12 @@
 
 @extends('layouts.app')
+@section('titulo') Catálogo de Publicaciones @endsection
+@extends('publicaciones.partials.breadcrumbs-biblioteca')
+@section('links')
+    <li>Catálogo de Publicaciones</li>
+@endsection
 @section('content')
     <div class="jumbotron">
-        <h1 class="text-center thin">Publicaciones</h1>
         <iframe src="http://104.131.14.98:1001/" frameborder="0" width="100%" height="2000px"></iframe>
     </div>
 
